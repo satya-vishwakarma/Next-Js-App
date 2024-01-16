7674f72b8a830f39c8ea42dd2d520a6b00ff14b6
satya-vishwakarma <satya9125@gmail.com>
2024-01-16 14:57:40 +0000

    adduser

--- UI/sub-components/students/AddStudent.js
+++ UI/sub-components/students/AddStudent.js
@@ -1,7 +1,30 @@
 // import node module libraries
-import { Card, Col, Form, Row } from 'react-bootstrap';
+import { Button, Card, Col, Form, Row } from 'react-bootstrap';
 
 const AddStudent = () => {
+
+  const classList = [1, 2, 3, 4, 5, 6, 7, 8]
+
+  function ordinal_suffix_of(i) {
+    let j = i % 10,
+      k = i % 100;
+    if (j === 1 && k !== 11) {
+      return i + "st";
+    }
+    if (j === 2 && k !== 12) {
+      return i + "nd";
+    }
+    if (j === 3 && k !== 13) {
+      return i + "rd";
+    }
+    return i + "th";
+  }
+
+
+
+
+
+
   return (
     <Row className="mb-8 justify-content-center">
 
@@ -44,6 +67,30 @@ const AddStudent = () => {
                 <Form.Label className="col-sm-4" htmlFor="fatherName">Mother Name</Form.Label>
                 <Col md={8} xs={12}>
                   <Form.Control type="text" placeholder="Enter your Mother name" id="motherName" required />
+                </Col>
+              </Row>
+
+
+              <Row className="mb-3">
+                <Form.Label className="col-sm-4" htmlFor="fatherName">Class</Form.Label>
+                <Col md={8} xs={12}>
+                  <Form.Select aria-label="Default select example">
+
+                    <option value="">Please select</option>
+                    {
+                      classList.map(i => (
+                        <option value={i}>{ordinal_suffix_of(i)}</option>
+                      )
+                      )
+
+                    }
+
+                  </Form.Select>
+                </Col>
+                <Col md={{ offset: 4, span: 8 }} xs={12} className="mt-4">
+                  <Button variant="primary" type="submit">
+                    Save Changes
+                  </Button>
                 </Col>
               </Row>
 
